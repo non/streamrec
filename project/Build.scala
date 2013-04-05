77e6899f3fe2190e448235a1dd7d452bf3029ef5
Erik Osheim <d_m@plastic-idolatry.com>
2013-04-04 22:52:00 -0400

    Move back to 2.10

--- project/Build.scala
+++ project/Build.scala
@@ -3,12 +3,18 @@ import Keys._
 
 object BuildSettings {
   val buildSettings = Defaults.defaultSettings ++ Seq(
-    organization := "org.scalamacros",
-    version := "1.0.0",
+    // organization := "org.scalamacros",
+    // version := "1.0.0",
+    // scalacOptions ++= Seq("-deprecation"),
+    // scalaVersion := "2.11.0-SNAPSHOT",
+    // scalaOrganization := "org.scala-lang.macro-paradise",
+    // resolvers += Resolver.sonatypeRepo("snapshots")
+
+    version := "0.1.0",
     scalacOptions ++= Seq("-deprecation"),
-    scalaVersion := "2.11.0-SNAPSHOT",
-    scalaOrganization := "org.scala-lang.macro-paradise",
-    resolvers += Resolver.sonatypeRepo("snapshots")
+    scalaVersion := "2.10.0",
+
+    libraryDependencies += "org.spire-math" %% "spire" % "0.4.0-M3"
   )
 }
 
@@ -25,7 +31,8 @@ object MyBuild extends Build {
     "macros",
     file("macros"),
     settings = buildSettings ++ Seq(
-      libraryDependencies <+= (scalaVersion)("org.scala-lang.macro-paradise" % "scala-reflect" % _))
+      libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _)
+    )
   )
 
   lazy val core: Project = Project(
